10237e376a17cdc47f717e671f2cc73826f09284
Zachary <tlyzachary0411@gmail.com>
2026-03-16 21:32:08 +0800

    added quote

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -465,7 +465,7 @@ const PageHome = () => {
                 handle: "@thirtyseven",
                 role: "Backend Dev",
                 bio: "Good at writing and doing backend stuff.",
-                quote: "{insert wise quote here}",
+                quote: "also i hate whatever slack is using for spellcheck",
                 photo: graysonPfp,
               },            ].map(({ initials, color, name, handle, role, bio, quote, photo }) => (
               <div
